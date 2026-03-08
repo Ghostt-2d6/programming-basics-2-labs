7cd46f808063089baf3342576f1cc132659ba3b3
Viktor <zazuliaviktor4@gmail.com>
2026-03-08 19:34:30 +0200

    a small PR2 clean-up

--- PR2/examples/demo.py
+++ PR2/examples/demo.py
@@ -1,5 +1,7 @@
 from fib_lib.generator import fibonacci
 from fib_lib.iterator import timeout_iterator
 
+#Демонстрація роботи бібліотеки fib_lib
 gen = fibonacci()
+#Запуск ітератора на 5 секунд
 timeout_iterator(5,gen)
