ed538846c0e0e2ddf29dea1df1f565581ad10756
Viktor <zazuliaviktor4@gmail.com>
2026-03-22 19:21:13 +0200

    PR3: added memoization functionality and LRU policy

--- PR3/pr3.py
+++ PR3/pr3.py
@@ -1,31 +1,59 @@
 import time
 
-def fibonacci_memo(func):
-    cache = {}
+def func_memo(max_cache=2, policy='LRU'):
 
-    def wrapper(*args):
-        if args in cache:
-           print("Returning from cache:",{cache[args]})
-           return cache[args]
-        else:
-            print("Processing...")
-            result = func(*args)
+    def func_decorator(func):
+        cache = {}
+
+        def lru_eject(cache, *args):
+                first_key = next(iter(cache))
+                print(f"Cache is full, removing oldest entry with this key: {first_key}")
+                del cache[first_key]
+
+        """ def lfu_eject(): """
+            
+        policies = {
+            'LRU': lru_eject
+        }
+
+        def func_wrapper(*args):
+            print(f"Current cache entries: {cache}")
+
+            if args in cache:
+                print("Returning from cache:",{cache[args]})
+
+                result = cache.pop(args)
+                cache[args] = result
+
+                return cache[args]
+            
+
+            else:
+                print("Processing...")
+                result = func(*args)
+
+                if len(cache) >= max_cache and max_cache != None:
+                    eject_func = policies.get(policy, policy)
+                    if callable(eject_func):
+                         eject_func(cache, *args)
+                         
+
+                cache[args] = result
+
+            return result
         
-        cache[args] = result
-
-        print(f"Current cache entries: {cache}")
-
-        return result
+        return func_wrapper
     
-    return wrapper
+    return func_decorator
 
 
-@fibonacci_memo
+@func_memo()
 def task(n):
     time.sleep(1)
     return n*10
 
 print(f"First test: {task(5)}")
-print(f"Cache test: {task(5)}")
 print(f"Second test: {task(10)}")
-print(f"Cache test: {task(10)}")
+print(f"Third test: {task(11)}")
+print(f"Fourth test: {task(5)}")
+print(f"Fifth test: {task(5)}")
